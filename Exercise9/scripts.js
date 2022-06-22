ab308d41c0a1ae493dc396914701521b75880157
EricLingyiWang <97856158+EricLingyiWang@users.noreply.github.com>
2022-06-21 20:00:44 -0600

    -

---
+++ Exercise9/scripts.js
@@ -0,0 +1,8 @@
+function funct1on(){
+  const stuff = prompt('Enter whatever you want');
+    alert("look what you've entered: " + stuff);
+}
+
+function funct10n(){
+  alert("nothing happens, what did you expect?");
+}
